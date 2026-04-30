65e236f3352b54e8868310aef67dd52e3b8b553d
Fayad <fayad@Fayads-MacBook-Air-932.local>
2026-04-30 14:06:07 +0530

    hydration fixed

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ import MobileBottomNav from '../components/MobileBottomNav';
 import CartSidebar from '../components/CartSidebar';
 import LoginBottomSheetWrapper from '../components/LoginBottomSheetWrapper';
 import ServiceAreaBottomSheet from '../components/ServiceAreaBottomSheet';
+import ClientOnly from '../components/ClientOnly';
 
 export const metadata = {
   title: 'Yaadro - Professional Supermarket Ecommerce',
@@ -34,37 +35,39 @@ export default function RootLayout({ children }) {
         className="flex flex-col min-h-screen bg-gray-50 overflow-x-hidden w-full max-w-full"
         style={{ overflowX: 'hidden', maxWidth: '100vw' }}
       >
-        <QueryProvider>
-          <AuthProvider>
-            <AlertProvider>
-              <CartProvider>
-                <WishlistProvider>
-                  <AddressProvider>
-                    <ActivityLogProvider>
-                      <RecentlyViewedProvider>
-                        <OrderProvider>
-                          <BottomNavVisibilityProvider>
-                            <LayoutHeightsProvider>
-                              <LocationServiceProvider>
-                                <ConditionalLayout>
-                                  {children}
-                                </ConditionalLayout>
-                                <MobileBottomNav />
-                                <CartSidebar />
-                                <LoginBottomSheetWrapper />
-                                <ServiceAreaBottomSheet />
-                              </LocationServiceProvider>
-                            </LayoutHeightsProvider>
-                          </BottomNavVisibilityProvider>
-                        </OrderProvider>
-                      </RecentlyViewedProvider>
-                    </ActivityLogProvider>
-                  </AddressProvider>
-                </WishlistProvider>
-              </CartProvider>
-            </AlertProvider>
-          </AuthProvider>
-        </QueryProvider>
+        <ClientOnly fallback={<div className="min-h-screen w-full bg-gray-50" />}>
+          <QueryProvider>
+            <AuthProvider>
+              <AlertProvider>
+                <CartProvider>
+                  <WishlistProvider>
+                    <AddressProvider>
+                      <ActivityLogProvider>
+                        <RecentlyViewedProvider>
+                          <OrderProvider>
+                            <BottomNavVisibilityProvider>
+                              <LayoutHeightsProvider>
+                                <LocationServiceProvider>
+                                  <ConditionalLayout>
+                                    {children}
+                                  </ConditionalLayout>
+                                  <MobileBottomNav />
+                                  <CartSidebar />
+                                  <LoginBottomSheetWrapper />
+                                  <ServiceAreaBottomSheet />
+                                </LocationServiceProvider>
+                              </LayoutHeightsProvider>
+                            </BottomNavVisibilityProvider>
+                          </OrderProvider>
+                        </RecentlyViewedProvider>
+                      </ActivityLogProvider>
+                    </AddressProvider>
+                  </WishlistProvider>
+                </CartProvider>
+              </AlertProvider>
+            </AuthProvider>
+          </QueryProvider>
+        </ClientOnly>
       </body>
     </html>
   );
